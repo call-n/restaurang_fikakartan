0fd3ba053dfa87be2e21d92ea447ba1569374636
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-10-03 22:01:22 +0200

    removed random blankspace in the middle of request...

--- src/services/MapAPI.js
+++ src/services/MapAPI.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 const getLatLong = async (address) => {
-    const res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?
-    address=${address}&key=${mapsApiKey}`)
+    const res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${mapsApiKey}`)
 
     const cords = res.data.results[0].geometry.location
-    
+
+    console.log(cords)
     return cords
 }
 
