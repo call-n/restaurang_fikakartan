5adc5d36f70a699f6689cccb8dc994a25d81336e
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-10-04 02:16:35 +0200

    Get users current location and show marker on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import {useState} from 'react'
 import mapAPI from '../services/MapAPI'
+import Button from 'react-bootstrap/Button'
 import Search from './Search'
-import {GoogleMap, useJsApiLoader} from '@react-google-maps/api'
+import {GoogleMap, useJsApiLoader, Marker} from '@react-google-maps/api'
 
 const libraries = ['places']
 
 
 const mapContainer = {
-    widht: '50vw',
-    height: '50vh'
+    widht: '100vw',
+    height: '100vh'
 }
 
 const Map = () => {
@@ -18,8 +19,18 @@ const Map = () => {
     const { isLoaded } = useJsApiLoader({mapsAPIKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,libraries})
 
     const [pos, setPos] = useState({lat: 56.3768708, lng: 13.9306438})
+    const [userPos, setUserPos] = useState("")
 
 
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const userPos={
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            }
+            setUserPos(userPos)
+        })
+    }
 
     const handleSubmit = async (address) => {
         const cords = await mapAPI.getLatLong(address)
@@ -37,12 +48,17 @@ const Map = () => {
         {isLoaded && (
             <>
                 <Search onSubmit={handleSubmit} />
+                <Button onClick={getCurrentLocation}>Find Me</Button>
 
                 <GoogleMap
                     center={pos}
                     mapContainerStyle={mapContainer}
                     zoom={12}
                 >
+
+                    {userPos && (
+                        <Marker position={userPos}/>
+                    )}
 
                 </GoogleMap>
 
