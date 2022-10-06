3c19ed12d79c8a57d389f82085d1b845a2e0ebd3
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-10-06 19:20:32 +0200

    resolved some issues after merge

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import UpdateRestaurantPage from './pages/UpdateRestaurantPage'
 import TipsPage from './pages/TipsPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
 import PageNotFound from './pages/PageNotFound'
 
 function App() {
@@ -19,18 +18,20 @@ function App() {
   return (
     <div id='App'>
       {authIsReady && (
-        <Navigation/>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
+        <>
+          <Navigation/>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
 
-          <Route path='update-restaurant/:id' element={<UpdateRestaurantPage/>}/>
-          <Route path='/create-restaurant' element={<CreateRestaurantPage/>}/>
-          <Route path='restaurant/:id' element={<RestaurantPage/>}/>
-          <Route path='/restaurants' element={<RestaurantsPage/>}/>
-          <Route path='/tips' element={<TipsPage/>} />
-          <Route path="*" element={<NotFound />} />
-          
-        </Routes>
+            <Route path='update-restaurant/:id' element={<UpdateRestaurantPage/>}/>
+            <Route path='/create-restaurant' element={<CreateRestaurantPage/>}/>
+            <Route path='restaurant/:id' element={<RestaurantPage/>}/>
+            <Route path='/restaurants' element={<RestaurantsPage/>}/>
+            <Route path='/tips' element={<TipsPage/>} />
+            <Route path="*" element={<PageNotFound />} />
+            
+          </Routes>
+        </>
       )}
       </div>
   )
